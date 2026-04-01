6afee3c4447850dd7445585160974a9deb333718
Yaşar Kumru <yasar.kumru@adalet.gov.tr>
2026-04-01 16:40:42 +0300

    install some packages, remove sunshine

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,16 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y \
+    htop \
+    git \
+    fastfetch \
+    vim \
+    kcalc
+
+
+rpm-ostree override remove \
+    sunshine
 
 # Use a COPR Example:
 #
